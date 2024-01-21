0455abbe01d13448b0b02cfe244c545638207daf
Ryan Su <suxi@oregonstate.edu>
2024-01-20 17:21:47 -0800

    Ignore:Test Run

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Test Run FoodTinder</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptatum quas, dolorem doloribus incidunt autem itaque sint vero veniam, optio quibusdam, mollitia praesentium! Fuga, quidem. Error nihil sequi rerum aut!</p>
     </>
   )
 }
